e2f9ceb2768d8c0df6913941aaae1cd436f43e0b
mfd <mfd@DESKTOP-KVPF9GU>
2022-12-14 04:20:51 +0300

    Add project files.

---
+++ HotelFinder.DataAccess/Concrete/HotelRepository.cs
@@ -0,0 +1,58 @@
+﻿using HotelFinder.DataAccess.Abstract;
+using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HotelFinder.DataAccess.Concrete
+{
+    public class HotelRepository : IHotelRepository
+    {
+        
+        public List<Hotel> GetAllHotels()
+        {
+            using(var dbContext= new HotelDbContext())
+            {
+                return dbContext.Hotels.ToList();
+            }
+        }
+        public Hotel GetHotelById(int id)
+        {
+            using (var dbContext = new HotelDbContext())
+            {
+                return dbContext.Hotels.Find(id);
+            }
+        }
+        public void DeleteHotel(int id)
+        {
+            using (var dbContext = new HotelDbContext())
+            {
+                var deletedHotel = GetHotelById(id);
+                dbContext.Hotels.Remove(deletedHotel);
+                dbContext.SaveChanges();
+            }
+        }
+        public Hotel CreateHotel(Hotel hotel)
+        {
+            using (var dbContext = new HotelDbContext())
+            {
+                dbContext.Hotels.Add(hotel);
+                dbContext.SaveChanges();
+
+                return hotel;
+            }
+        }
+
+        public Hotel UpdateHotel(Hotel hotel)
+        {
+            using (var dbContext = new HotelDbContext())
+            {
+                dbContext.Hotels.Update(hotel);
+                dbContext.SaveChanges();
+
+                return hotel;
+            }
+        }
+    }
+}
---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,16 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer($"Server=DESKTOP-KVPF9GU;Database=HotelDb;Trusted_Connection=True;Connect Timeout=30;MultipleActiveResultSets=True;");
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
